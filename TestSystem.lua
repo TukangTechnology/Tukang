7e72558885bf5a86e41922158a6a40517b56d86a
EnzoBRZ <79358946+TukangTechnology@users.noreply.github.com>
2023-04-05 21:36:54 +0800

    Delete TestSystem.lua

--- TestSystem.lua
+++
@@ -1,4 +0,0 @@
-if game.PlaceId == 12851888521 then game.Players.LocalPlayer:Kick("Detected Punch Simulator!")
-  elseif game.PlaceId == 2788229376 then game.Players.LocalPlayer:Kick("Detected DaHood!")
-  end
-end
