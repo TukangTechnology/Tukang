feb1bdb98619be028f1a14db5b5808eb6510f0cd
EnzoBRZ <79358946+TukangTechnology@users.noreply.github.com>
2023-04-07 16:49:33 +0800

    Update WLLoader.lua

--- WLLoader.lua
+++ WLLoader.lua
@@ -1,14 +1,1 @@
-if game.PlaceId == 12851888521 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TukangTechnology/Tukang/main/WLPunchingWallSimulator.lua",true))()
-end
-if game.PlaceId == 2788229376 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TukangTechnology/Tukang/main/WLDaHood.lua",true))()
-end
-if game.PlaceId == 23578803 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TukangTechnology/Tukang/main/WLHotelElephant.lua",true))()
-end
-if game.PlaceId == 189707 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TukangTechnology/Tukang/main/WLNaturalDisasters.lua",true))()
-end
-elseif game.Players.LocalPlayer:Kick("Game Not Supported!")
-end
+
