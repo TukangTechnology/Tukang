164cffb2a0b78a73ea7e30f0117b2dddd3c1761d
EnzoBRZ <79358946+TukangTechnology@users.noreply.github.com>
2023-04-07 16:18:21 +0800

    Update WLLoader.lua

--- WLLoader.lua
+++ WLLoader.lua
@@ -1,1 +1,12 @@
-
+if game.PlaceId == 12851888521 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TukangTechnology/Tukang/main/WLPunchingWallSimulator.lua",true))()
+end
+if game.PlaceId == 2788229376 then
+    print("Testing Lua")
+end
+if game.PlaceId == 23578803 then
+    print("Testing Lua")
+end
+if game.PlaceId == 189707 then
+    print("Testing Lua")
+end
