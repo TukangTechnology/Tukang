8927dfd10f303e32d8b6cc373e17274e9ea932cf
EnzoBRZ <79358946+TukangTechnology@users.noreply.github.com>
2023-04-08 12:24:28 +0800

    Update WLLoader.lua

--- WLLoader.lua
+++ WLLoader.lua
@@ -4,9 +4,6 @@ end
 if game.PlaceId == 189707 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TukangTechnology/Tukang/main/WLNaturalDisasters.lua",true))()
 end
-if game.PlaceId == 23578803 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TukangTechnology/Tukang/main/WLHotelElephant.lua",true))()
-end
 if game.PlaceId == 2788229376 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TukangTechnology/Tukang/main/WLDaHood.lua",true))()
 end
